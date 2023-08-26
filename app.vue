49a4bb203a404b4e016eb8873de1fb8250b7a8c0
Itay Sagui <itsagui@microsoft.com>
2023-08-26 16:08:11 +0300

    Added Google analytics

--- app.vue
+++ app.vue
@@ -114,7 +114,14 @@
       </div>
     </div>
   </div>
-  
+  <!-- Google tag (gtag.js) -->
+  <script async src="https://www.googletagmanager.com/gtag/js?id=G-SXBT5PD8V5"></script>
+  <script>
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+    gtag('config', 'G-SXBT5PD8V5');
+  </script>
 </template>
 <script setup lang="ts">
 useHead({ title: 'Workouts AI - Create Workouts in Seconds with AI' })
